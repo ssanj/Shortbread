4da5fc6179c3e1ad43da17208d57aa14fdbe904e
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-01-24 16:20:48 +1000

    more refactoring of DriverConfig

--- src/main/scala/DriverConfig.scala
+++ src/main/scala/DriverConfig.scala
@@ -6,27 +6,27 @@ package ssahayam
 
 import org.openqa.selenium.remote.RemoteWebDriver
 import java.util.concurrent.TimeUnit
-import java.util.concurrent.TimeUnit._
 
+//TODO: 1. MOve this into an IO package
 trait DriverConfig extends PluginSupport {
 
   type Timeout = (Long, TimeUnit)
 
   val profile:String
-  val pageTimeout:Timeout
-  val scriptTimeout:Timeout
-  def driver:NamedDriver
+  val pageTimeout:Option[Timeout]
+  val scriptTimeout:Option[Timeout]
+  val useDefaultTimeout:Option[Timeout] = None
+  def webDriver:NamedDriver
 
-  //Although each of the timeout functions may throw exception, since they run in an IO context they will be handled appropriately
-  def setPageTimeout: (RemoteWebDriver) => Unit = { setTimeout(_)(pf)(pageTimeout) }
+  def setPageTimeout: (RemoteWebDriver) => Unit = {
+    driver => setTimeout(pageTimeout)(t => driver.manage.timeouts.implicitlyWait(t._1, t._2))
+  }
 
-  def setScriptTimeout: (RemoteWebDriver) => Unit = { setTimeout(_)(sf)(scriptTimeout) }
+  def setScriptTimeout: (RemoteWebDriver) => Unit = {
+    driver => setTimeout(scriptTimeout)(t => driver.manage.timeouts.setScriptTimeout(t._1, t._2))
+  }
 
-  def pf(driver:RemoteWebDriver, t:Timeout): Unit = driver.manage.timeouts.implicitlyWait(t._1, t._2)
-
-  def sf(driver:RemoteWebDriver, t:Timeout): Unit = driver.manage.timeouts.setScriptTimeout(t._1, t._2)
-
-  def setTimeout(driver:RemoteWebDriver): ((RemoteWebDriver, Timeout) => Unit) => Timeout => Unit = f => t => f(driver, t) 
+  def setTimeout(timeout:Option[Timeout])(f:(Timeout) => Unit) { runSafelyWithOptionReturnError(timeout.map(f)) }
 }
 
 //f() is a side-effecting function that launches a browser/driver.
@@ -34,14 +34,15 @@ case class NamedDriver(name:String, f: () => RemoteWebDriver)
 
 object DefaultFoxConfig extends DriverConfig {
 
-  override lazy val profile = "default"
-  override lazy val pageTimeout = (5L, SECONDS)
-  override lazy val scriptTimeout = (5L, SECONDS)
+  import java.util.concurrent.TimeUnit._
+  import org.openqa.selenium.firefox.FirefoxDriver
+  import org.openqa.selenium.firefox.internal.ProfilesIni
 
-  override def driver = NamedDriver("Firefox", () => {
-    import org.openqa.selenium.firefox.FirefoxDriver
-    import org.openqa.selenium.firefox.internal.ProfilesIni
-  
+  override lazy val profile = "default"
+  override lazy val pageTimeout = useDefaultTimeout
+  override lazy val scriptTimeout = Some(5L, SECONDS)
+
+  override def webDriver = NamedDriver("Firefox", () => {
     val ffd = new FirefoxDriver(new ProfilesIni().getProfile(profile))
     setPageTimeout(ffd)
     setScriptTimeout(ffd)
@@ -50,18 +51,18 @@ object DefaultFoxConfig extends DriverConfig {
 }
 
 object DefaultChromeConfig extends DriverConfig {
-
+  import java.util.concurrent.TimeUnit._
   override lazy val profile = "default"
-  override lazy val pageTimeout = (5L, SECONDS)
-  override lazy val scriptTimeout = (5L, SECONDS)
+  override lazy val pageTimeout = useDefaultTimeout
+  override lazy val scriptTimeout = Some(5L, SECONDS)
 
-  override def driver = NamedDriver("Chrome", () => {
-     import org.openqa.selenium.chrome.ChromeDriver
- 
-     val chrome = new ChromeDriver
-     setPageTimeout(chrome)
-     setScriptTimeout(chrome)
-     chrome
+  override def webDriver = NamedDriver("Chrome", () =>
+    {
+      import org.openqa.selenium.chrome.ChromeDriver
+      val chrome = new ChromeDriver
+      setPageTimeout(chrome)
+      setScriptTimeout(chrome)
+      chrome
   })
-}
 
+}
